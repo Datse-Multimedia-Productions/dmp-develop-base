b859093938137507adfda18d7c7e1148e94cc249
JigmeDatse <info@datsemultimedia.com>
2015-09-04 22:05:37 -0700

    Copied current index.html to the git repositry.
    Created view/header.php and view/footer.php as default headers and footers.
    
    Created index.php to include header and footer, and put other stuff from index.html that got excluded.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * Main view of the DMP Develop project
+ *
+ * This is the main view of the DMP Develop Project.  This is the Base of the program.  
+ *
+ * @copyright 2015 Datse Multimedia Productions
+ * @license GNU General Public License, version 3
+ * @author Jigme Datse Yli-Rasku <jigme.datse@dmp-develop.com>
+ * @version 0.0.1
+ * 
+ * @pacakge DMP-Develop/base/view
+ * 
+ */
+
+require-once(view/header.php);
+echo "
+		<h1>DMP Development</h1>
+		<p>If you're here and seeing this, you've got here too soon</p>
+";
+require-once(view/footer.php);
+?>
